0f97764a1ac9265b142e5c12c362fc7a7f76e9bb
Sagitov Eldar <salvadore1987@gmail.com>
2023-01-20 19:23:25 +0500

    Ref IncidentTask

--- src/main/java/uz/keysoft/camunda/spring/boot/starter/dto/incident/IncidentTask.java
+++ src/main/java/uz/keysoft/camunda/spring/boot/starter/dto/incident/IncidentTask.java
@@ -1,15 +1,11 @@
 package uz.keysoft.camunda.spring.boot.starter.dto.incident;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import uz.keysoft.camunda.spring.boot.starter.serializers.OffsetDateTimeDeserializer;
-
-import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -22,8 +18,7 @@ public class IncidentTask {
   String processDefinitionId;
   String processInstanceId;
   String executionId;
-  @JsonDeserialize(using = OffsetDateTimeDeserializer.class)
-  LocalDateTime incidentTimestamp;
+  String incidentTimestamp;
   String incidentType;
   String activityId;
   String failedActivityId;
